cd633598b25bb4f7e00506afd915e713e5e11258
SennoTaphoorn <29040@ma-web.nl>
2021-03-11 12:11:18 +0100

    First commit

---
+++ Les-01/Functies/AchievementFunctions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Achievement1
+{
+    class Program
+    {
+        static void HW()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static void Main(string[] args)
+        {
+            HW();
+        }
+
+    }
+}
---
+++ Les-01/HelloWorld/AchievementHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AchievementHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
